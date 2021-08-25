93cff3344e1f1bbda08256f319a9370c471763b7
hwalima <hwalima@outlook.com>
2021-08-25 21:43:24 +0200

    Update welcome.js

--- JAvaScript/welcome.js
+++ JAvaScript/welcome.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Sikhulile Here!";
+var myName = "Sikhulile here!";
 var letterColors = [red, orange, green];
 if ( 10 > 3) {
     bubbleShape="circle";
